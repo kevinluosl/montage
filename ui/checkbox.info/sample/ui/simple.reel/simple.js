b5b38e00bdbda53d2503134310b34f09894b936b
tom <tom.wang@siloon.com>
2016-01-18 11:27:22 +0800

    Adding checkbox sample

---
+++ ui/checkbox.info/sample/ui/simple.reel/simple.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/simple.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Simple
+ * @extends Component
+ */
+exports.Simple = Component.specialize(/** @lends Simple# */ {
+    constructor: {
+        value: function Simple() {
+            this.super();
+        }
+    }
+});
