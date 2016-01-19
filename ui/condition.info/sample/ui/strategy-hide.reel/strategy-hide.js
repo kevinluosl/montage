6fd3e0276fc1652fcf1dbe8bdaec3924deeeaba7
tom <tom.wang@siloon.com>
2016-01-19 15:06:45 +0800

    condition sample

---
+++ ui/condition.info/sample/ui/strategy-hide.reel/strategy-hide.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/strategy-hide.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class StrategyHide
+ * @extends Component
+ */
+exports.StrategyHide = Component.specialize(/** @lends StrategyHide# */ {
+    constructor: {
+        value: function StrategyHide() {
+            this.super();
+        }
+    }
+});
