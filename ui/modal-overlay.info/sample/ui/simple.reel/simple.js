fd355400e11c30a116c0c0677621994496bef9af
tom <tom.wang@siloon.com>
2016-01-19 15:17:50 +0800

    modal-overlay sample

---
+++ ui/modal-overlay.info/sample/ui/simple.reel/simple.js
@@ -0,0 +1,22 @@
+/**
+ * @module ui/simple.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Simple
+ * @extends Component
+ */
+exports.Simple = Component.specialize(/** @lends Simple# */ {
+    constructor: {
+        value: function Simple() {
+            this.super();
+        }
+    },
+
+    handleAction:{
+        value:function(){
+           this.templateObjects.overlay.show();
+        }
+    }
+});
